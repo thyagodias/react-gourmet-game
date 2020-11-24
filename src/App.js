7a83ea48ec5e6ddf4cd049841e2bcd54d8fbc1a0
Thyago Dias <thyagojuniorrochadias@gmail.com>
2020-11-24 17:46:10 -0400

    feat: adiciona configuração de rotas

--- src/App.js
+++ src/App.js
@@ -1,52 +1,10 @@
 import React from 'react';
-import Game from './pages/Game';
+import Routes from './routes';
 
 function App() {
-  // let round = 0;
-  // const state = [
-  //   {
-  //     pergunta: 'massa',
-  //     chute: 'macarrao',
-  //     proximaPergunta: 'bolo',
-  //   },
-  //   {
-  //     pergunta: 'bolo',
-  //     chute: null,
-  //     proximaPergunta: null,
-  //   },
-  // ];
-
-  // console.log(state[round]);
-
-  // function chutar() {
-  //   console.log('sue prato é', state[round].chute);
-  // }
-
-  // function handleAddResposta(parentQuestion) {
-  //   state[round] = {
-  //     pergunta: 'Nova Pergunta',
-  //     chute: 'Novo chute',
-  //     proximaPergunta: parentQuestion,
-  //   };
-
-  //   round = 0;
-  //   console.log('vou Adicionar uma nova resposta para', parentQuestion);
-  // }
-
-  // function handleNextPergunta() {
-  //   round += 1;
-  //   if (!state[round].proximaPergunta) {
-  //     chutar();
-  //     handleAddResposta(state[round].pergunta);
-  //     round = 0;
-  //     return;
-  //   }
-  //   console.log(state);
-  // }
-
   return (
     <div className="App">
-      <Game />
+      <Routes />
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <>
+      <h1>Bem vindo ao Jogo Gourmet</h1>
+      <h4>Gostaria de tentar adivinhar o prato em que você pensou</h4>
+      <Link itemType="button" to="/game">
+        COMEÇAR
+      </Link>
+    </>
+  );
+};
+
+export default Home;
