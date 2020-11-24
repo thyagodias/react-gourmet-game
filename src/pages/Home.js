ab3409f6908718b6d110b4bf813544b2af173402
Thyago Dias <thyagojuniorrochadias@gmail.com>
2020-11-24 17:48:39 -0400

    chore: adiciona mensagem na página inicial

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ const Home = () => {
     <>
       <h1>Bem vindo ao Jogo Gourmet</h1>
       <h4>Gostaria de tentar adivinhar o prato em que você pensou</h4>
+      <h4>Antes de começar, pense em um prato ...</h4>
       <Link itemType="button" to="/game">
         COMEÇAR
       </Link>
